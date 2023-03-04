3ef89546b42a6c536ec53afd5aea61ad26ee5083
dinu.garbuz gmail.com <dinu.garbuz@gmail.com>
2023-03-04 19:28:52 +0200

    contact us page

---
+++ Kickbox/Kickbox/ClientApp/src/app/contactus/contactus.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contactus',
+  templateUrl: './contactus.component.html',
+  styleUrls: ['./contactus.component.css'],
+})
+export class ContactUsComponent {
+}
+
+// Initialize and add the map
+function initMap(): void {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.031 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(
+    document.getElementById("map") as HTMLElement,
+    {
+      zoom: 4,
+      center: uluru,
+    }
+  );
+
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+declare global {
+  interface Window {
+    initMap: () => void;
+  }
+}
+window.initMap = initMap;
